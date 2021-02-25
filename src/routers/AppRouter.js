3260c6fcb19990a0c11dc1e990dd4d4165290b11
YTRodi <yagorodi@gmail.com>
2021-02-25 03:05:33 -0300

    add new route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import HomeScreen from '../components/HomeScreen';
+import AddNewPost from '../components/ui/AddNewPost';
 import Navbar from '../components/ui/Navbar';
 
 const AppRouter = () => {
@@ -12,7 +13,7 @@ const AppRouter = () => {
 
 				<Switch>
 					<Route exact path='/home' component={HomeScreen} />
-					<Route exact path='/add' component={HomeScreen} />
+					<Route exact path='/add' component={AddNewPost} />
 
 					<Redirect to='/home' />
 				</Switch>
